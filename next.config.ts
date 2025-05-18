c1f87e18aae63c1a9b70870e6037ca43adc5a520
Yuhang Zhang <yuhangz@bu.edu>
2025-05-18 14:48:44 -0400

    debug

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: [
+    'http://192.168.56.1:3000',
+    'http://localhost:3000',
+  ],
+  webpack(config, { dev, isServer }) {
+    if (dev && !isServer) {
+      config.output.publicPath = '/_next/static/webpack/';
+    }
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
