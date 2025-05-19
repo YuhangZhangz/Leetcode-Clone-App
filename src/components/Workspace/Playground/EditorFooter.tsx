08fece4957edf2eac13b85d4178b0013c1895ebf
Yuhang Zhang <yuhangz@bu.edu>
2025-05-19 16:51:39 -0400

    feat(frontend): add complete ProblemPage UI

---
+++ src/components/Workspace/Playground/EditorFooter.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BsChevronUp } from 'react-icons/bs';
+
+const EditorFooter: React.FC = () => {
+  return (
+    <div className="flex bg-neutral-800 absolute bottom-0 z-10 w-full">
+      <div className="mx-5 my-[10px] flex justify-between w-full">
+        <div className="mr-2 flex flex-1 flex-nowrap items-center space-x-4">
+          <button className="px-3 py-1.5 font-medium items-center transition-all inline-flex bg-neutral-700 text-sm hover:bg-neutral-600 text-neutral-400 rounded-lg">
+            Console
+            <div className="ml-1 transform transition flex items-center">
+              <BsChevronUp className="fill-gray-600 mx-1" />
+            </div>
+          </button>
+        </div>
+        <div className="ml-auto flex items-center space-x-4">
+          <button className="px-3 py-1.5 text-sm font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex bg-neutral-700 hover:bg-neutral-600 text-neutral-400 rounded-lg">
+            Run
+          </button>
+          <button className="px-3 py-1.5 font-medium items-center transition-all focus:outline-none inline-flex text-sm text-white bg-green-600 hover:bg-green-400 rounded-lg">
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditorFooter;
--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -4,8 +4,7 @@ import dynamic from 'next/dynamic';
 import PreferenceNav from './PreferenceNav/PreferenceNav';
 import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 import { javascript } from '@codemirror/lang-javascript';
-
-// 浏览器专属库必须动态加载并禁用 SSR
+import EditorFooter from './EditorFooter';
 const Split = dynamic(() => import('react-split'), { ssr: false });
 const CodeMirror = dynamic(
   () => import('@uiw/react-codemirror').then(mod => mod.default),
@@ -35,17 +34,17 @@ const Playground: React.FC<PlaygroundProps> = () => {
         </div>
 
         <div className="w-full px-5 overflow-auto">
-          {/* Test Case 标题 */}
+          {/* Test Case  head*/}
           <div className="flex h-10 items-center space-x-6">
             <div className="relative flex h-full flex-col justify-center cursor-pointer">
               <div className="text-sm font-medium leading-5 text-white">
                 Test Case
               </div>
-              <hr className="absolute bottom-0 h-0.5 w-16 rounded-full border-none bg-white" />
+              <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white" />
             </div>
           </div>
 
-          {/* Test Case 按钮 */}
+          {/* Test Case Button */}
           <div className="flex mt-2 space-x-2 text-white">
             {['Case 1', 'Case 2', 'Case 3'].map(label => (
               <div key={label} className="inline-flex items-center gap-y-4">
@@ -55,8 +54,23 @@ const Playground: React.FC<PlaygroundProps> = () => {
               </div>
             ))}
           </div>
+          <div className='font-semibold my-4'>
+            <p className='text-sm font-medium mt-4 text-white'>
+              Input:
+            </p>
+            <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-neutral-600 border-transparent text-white mt-2'>
+              nums:[2, 7, 11, 15],target: 9
+            </div>
+            <p className='text-sm font-medium mt-4 text-white'>
+              Output:
+            </p>
+            <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-neutral-600 border-transparent text-white mt-2'>
+              [0, 1]
+            </div>
+          </div>
         </div>
       </Split>
+      <EditorFooter />
     </div>
   );
 };
