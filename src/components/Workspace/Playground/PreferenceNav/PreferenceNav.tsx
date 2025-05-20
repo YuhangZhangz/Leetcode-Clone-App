457b6680115956771d994e144dc5694cbedc2612
Yuhang Zhang <yuhangz@bu.edu>
2025-05-20 18:55:36 -0400

    Fullscreen model

--- src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
+++ src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
@@ -1,10 +1,36 @@
 "use client";
-import React from 'react';
-import { AiOutlineFullscreen, AiOutlineSetting } from 'react-icons/ai';
+import { useState, useEffect } from 'react';
+import { AiOutlineFullscreen, AiOutlineFullscreenExit, AiOutlineSetting } from 'react-icons/ai';
 
 type PreferenceNavProps = {};
 
 const PreferenceNav: React.FC<PreferenceNavProps> = () => {
+  const [isFullScreen, setIsFullScreen] = useState(false)
+
+  const handleFullScreem = () => {
+    if(isFullScreen){
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+    setIsFullScreen(!isFullScreen)
+  }
+	useEffect(() => {
+		function exitHandler(e: any) {
+			if (!document.fullscreenElement) {
+				setIsFullScreen(false);
+				return;
+			}
+			setIsFullScreen(true);
+		}
+
+		if (document.addEventListener) {
+			document.addEventListener("fullscreenchange", exitHandler);
+			document.addEventListener("webkitfullscreenchange", exitHandler);
+			document.addEventListener("mozfullscreenchange", exitHandler);
+			document.addEventListener("MSFullscreenChange", exitHandler);
+		}
+	}, [isFullScreen]);
   return (
     <div className="flex items-center justify-between bg-gray-700 h-11 w-full">
       <div className="flex items-center text-white">
@@ -23,8 +49,12 @@ const PreferenceNav: React.FC<PreferenceNavProps> = () => {
           </div>
         </button>
 
-        <button className="relative group p-2 rounded hover:bg-gray-600">
-          <AiOutlineFullscreen className="h-4 w-4 text-gray-400" />
+        <button className="relative group p-2 rounded hover:bg-gray-600"
+        onClick={handleFullScreem}
+        >
+          <div className="h-4 w-4 text-gray-400">
+            {!isFullScreen ? <AiOutlineFullscreen />: <AiOutlineFullscreenExit />}
+          </div>
           <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 whitespace-no-wrap bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity">
             Full Screen
           </div>
